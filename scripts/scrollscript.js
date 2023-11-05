782ec9399fd0f4793f19780372eb49f017b7e394
Vaclav Vondracek <vondracek@spsejecna.cz>
2023-11-05 10:46:23 +0100

    navbar changing background depending on background

---
+++ scripts/scrollscript.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll", function () {
+    const midText = document.getElementById("mid-text");
+    const navbar = document.getElementById("navbar");
+   
+    const rect = midText.getBoundingClientRect();
+
+ 
+    if (rect.bottom <= 0) {
+        
+        navbar.classList.remove('lightnav');
+        navbar.classList.add('darknav');
+    } else {
+        navbar.classList.remove('darknav');
+        navbar.classList.add('lightnav');
+}});
