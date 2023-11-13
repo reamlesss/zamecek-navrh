e97e5fd2f74cdf24c15f11fc0d05efd194b1dcc3
Vaclav Vondracek <vondracek@spsejecna.cz>
2023-11-13 14:27:26 +0100

    fixed navbar for mobile

--- scripts/scrollscript.js
+++ scripts/scrollscript.js
@@ -1,6 +1,8 @@
 window.addEventListener("scroll", function () {
     const midText = document.getElementById("mid-text");
     const navbar = document.getElementById("navbar");
+    const navbg = document.getElementById('navbg');
+
    
     const rect = midText.getBoundingClientRect();
 
@@ -8,7 +10,11 @@ window.addEventListener("scroll", function () {
     if (rect.bottom < 5) {
         navbar.classList.remove('lightnav');
         navbar.classList.add('darknav');
+        navbg.classList.remove('lightnavbg');
+        navbg.classList.add('darknavbg');
     } else {
         navbar.classList.remove('darknav');
         navbar.classList.add('lightnav');
+        navbg.classList.remove('darknavbg');
+        navbg.classList.add('lightnavbg');
 }});
